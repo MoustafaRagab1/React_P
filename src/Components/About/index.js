186bd4757e427258a301e47196cfa54eeca9a620
MoustafaRagab1 <rmostafa890@gmail.com>
2022-03-19 11:00:35 +0400

    Uplaod

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -10,7 +10,9 @@ const About = () => {
           <h2 className="info-title"><span>This is</span> Me</h2>
           <h4 className="info-dir">Creative Director</h4>
           <p className="info-desc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est <a href="#">explicabo</a> maxime quibusdam quaerat nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad veritatis?
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est
+               {/* <a href="#">explicabo</a>  */}
+               maxime quibusdam quaerat nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad veritatis?
           </p>
           <p className="info-desc">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est explicabo maxime quibusdam quaerat nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad veritatis?
